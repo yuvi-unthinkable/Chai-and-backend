e11f52affd4ad3ffb28f66b72b4c0bd3e02793f0
yuvraj <yuvraj.tripathi@unthinkable.co>
2025-08-02 18:03:44 +0530

    util code added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import dotenv from 'dotenv';
 // import mongoose from 'mongoose';
 // import { DB_NAME } from './constants';
 import connectDB from './db/index.js';
+import express from 'express'
+
+const app = express()
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR ", error);
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("Mongo Db connection failed !!!", err);
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor (StatusCode, data, message = "Sucess") {
+        this.StatusCode = StatusCode
+        this.data = data
+        this.message = message
+        this.sucess = StatusCode < 400
+
+    }
+}
