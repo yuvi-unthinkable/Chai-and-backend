d159d0acc079d4f501a63f239d6cc3efdbff5530
yuvraj <yuvraj.tripathi@unthinkable.co>
2025-08-25 18:26:58 +0530

    samesite fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -153,7 +153,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: false,
-    sameSite: "None",
+    sameSite: "none",
   };
 
   return res
