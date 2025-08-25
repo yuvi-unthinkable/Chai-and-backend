f4e24c3fa929c92187a26152baa8487e01b812a5
yuvraj <yuvraj.tripathi@unthinkable.co>
2025-08-25 18:47:20 +0530

    added cors origin

--- src/app.js
+++ src/app.js
@@ -3,11 +3,22 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 
 const app = express()
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://lords-hotel.netlify.app'
+];
 
 app.use(cors({
-    origin : 'http://localhost:5173',
-    credentials :true
-}))
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like curl or Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
